cb5efc271534716dad7578e33ca4d6e6ec90362d
ChrisTimperley <christimperley@googlemail.com>
2020-08-25 20:35:31 -0400

    added AST caching and Python version to Program

--- src/apodora/program.py
+++ src/apodora/program.py
@@ -2,9 +2,11 @@
 __all__ = ('Program',)
 
 from typing import Mapping
+import functools
 import types
 
 import attr
+import parso
 
 
 @attr.s(slots=True, frozen=True)
@@ -13,8 +15,12 @@ class Program:
 
     Attributes
     ----------
+    python: str
+        The version of Python used by the program.
     module_to_source: Mapping[str, str]
         The source code for each module, indexed by module name.
+    module_to_ast: Mapping[str, parso.tree.NodeOrLeaf]
+        The AST for each module, indexed by module name.
     main_module: str
         The name of the Python module that provides the entrypoint for the
         program. For now, we do not consider method entrypoints.
@@ -24,7 +30,10 @@ class Program:
     ValueError
         If no source code has been provided for the main module.
     """
+    python: str = attr.ib()
     module_to_source: Mapping[str, str] = attr.ib(repr=False)
+    module_to_ast: Mapping[str, parso.tree.NodeOrLeaf] = \
+        attr.ib(repr=False, init=False)
     main_module: str = attr.ib(default='__main__')
 
     @main_module.validator
@@ -36,3 +45,11 @@ class Program:
     def __attrs_post_init__(self) -> None:
         module_to_source = types.MappingProxyType(self.module_to_source)
         object.__setattr__(self, 'module_to_source', module_to_source)
+
+        parse_ast = functools.partial(parso.parse, version=self.python)
+        module_to_ast: Mapping[str, parso.tree.NodeOrLeaf] = {
+            name: parse_ast(source) for (name, source)
+            in module_to_source.items()
+        }
+        module_to_ast = types.MappingProxyType(module_to_ast)
+        object.__setattr__(self, 'module_to_ast', module_to_ast)
