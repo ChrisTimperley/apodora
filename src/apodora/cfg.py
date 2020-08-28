19b9f2f19bba77cbaea6dab260d79f49c1b95313
ChrisTimperley <christimperley@googlemail.com>
2020-08-28 11:48:58 -0400

    updated cfg module

--- src/apodora/cfg.py
+++ src/apodora/cfg.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from typing import Deque, Dict
+__all__ = ('ModuleCFG', 'ProgramCFG')
+
 from collections import deque
+from typing import (AbstractSet, Deque, Dict, List, MutableSequence, Optional,
+                    Tuple)
+from typed_ast import ast27, ast3
 import typing
 
 from loguru import logger
@@ -10,13 +14,69 @@ if typing.TYPE_CHECKING:
     from .program import Program
 
 
+# TODO: ENTER/EXIT (i.e., source/sink)
+@attr.s(slots=True)
+class BasicBlock:
+    number: int = attr.ib()
+    statements = attr.ib(factory=list)
+    calls: AbstractSet[str] = attr.ib(factory=list)
+    # predecessors
+    # successors
+
+    def add_exit(self, exit: 'BasicBlock') -> None:
+        raise NotImplementedError
+
+
+@attr.s(slots=True)
+class ModuleCFGVisitor:
+    module: str = attr.ib()
+    namespace: str = attr.ib(init=False)
+
+    def __attrs_post_init__(self) -> None:
+        self.namespace = self.module
+
+
+class Py27ModuleCFGVisitor(ModuleCFGVisitor, ast27.NodeVisitor):
+    pass
+
+
+class Py3ModuleCFGVisitor(ModuleCFGVisitor, ast3.NodeVisitor):
+    pass
+
+
 @attr.s(slots=True)
 class ModuleCFG:
+    """
+    Attributes
+    ----------
+    program: Program
+        The program to which this CFG belongs.
+    module: str
+        The name of the module that this CFG represents.
+    imports: AbstractSet[str]
+        The fully-qualified names of the modules that are imported into this
+        module.
+    """
     program: 'Program' = attr.ib()
     module: str = attr.ib()
+    imports: AbstractSet[str] = attr.ib()
 
     @classmethod
     def build(cls, program: 'Program', module: str) -> 'ModuleCFG':
+        ast = program.module_to_ast[module]
+
+        visitor: ModuleCFGVisitor
+        if program.is_py2:
+            visitor = Py3ModuleCFGVisitor(module=module)
+        elif program.is_py3:
+            visitor = Py27ModuleCFGVisitor(module=module)
+        else:
+            m = f"unable to build CFG for Python version {program.python}"
+            raise ValueError(m)
+
+        visitor.visit(ast)
+
+        # TODO
         cfg = ModuleCFG(program=program, module=module)
         return cfg
 
