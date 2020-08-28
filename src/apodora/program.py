5d52485cf6280a4a1a4c92604b0b9f49b26f3933
ChrisTimperley <christimperley@googlemail.com>
2020-08-28 11:25:47 -0400

    use typed_ast rather than parso

--- src/apodora/program.py
+++ src/apodora/program.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 __all__ = ('Program',)
 
-from typing import AbstractSet, Mapping
+from typing import AbstractSet, Generic, Mapping, TypeVar
+from typed_ast import ast27, ast3
 import functools
 import types
 
 import attr
-import parso
+
+T = TypeVar('T', ast27.AST, ast3.AST)
 
 
 @attr.s(slots=True, frozen=True)
-class Program:
+class Program(Generic[T]):
     """Describes the program under analysis.
 
     Attributes
@@ -19,7 +21,7 @@ class Program:
         The version of Python used by the program.
     module_to_source: Mapping[str, str]
         The source code for each module, indexed by module name.
-    module_to_ast: Mapping[str, parso.tree.NodeOrLeaf]
+    module_to_ast: Mapping[str, T]
         The AST for each module, indexed by module name.
     modules: AbstractSet[str]
         The names of the modules for this program.
@@ -32,9 +34,9 @@ class Program:
     ValueError
         If no source code has been provided for the main module.
     """
-    python: str = attr.ib()
+    python: str = attr.ib(validator=attr.validators.instance_of(str))
     module_to_source: Mapping[str, str] = attr.ib(repr=False)
-    module_to_ast: Mapping[str, parso.tree.NodeOrLeaf] = \
+    module_to_ast: Mapping[str, 'T'] = \
         attr.ib(repr=False, init=False)
     modules: AbstractSet[str] = attr.ib(init=False, repr=False)
     main_module: str = attr.ib(default='__main__')
@@ -45,14 +47,22 @@ class Program:
             msg = f'source not provided for main module [{value}]'
             raise ValueError(msg)
 
-    def __attrs_post_init__(self) -> None:
-        module_to_source = types.MappingProxyType(self.module_to_source)
-        object.__setattr__(self, 'module_to_source', module_to_source)
+    @property
+    def is_py2(self) -> bool:
+        return self.python.startswith('2.')
 
+    @property
+    def is_py3(self) -> bool:
+        return self.python.startswith('3.')
+
+    def __attrs_post_init__(self) -> None:
         modules: AbstractSet[str] = frozenset(self.module_to_source)
         object.__setattr__(self, 'modules', modules)
 
-        parse_ast = functools.partial(parso.parse, version=self.python)
+        module_to_source = types.MappingProxyType(self.module_to_source)
+        object.__setattr__(self, 'module_to_source', module_to_source)
+
+        parse_ast = ast27.parse if self.is_py2 else ast3.parse
         module_to_ast: Mapping[str, parso.tree.NodeOrLeaf] = {
             name: parse_ast(source) for (name, source)
             in module_to_source.items()
