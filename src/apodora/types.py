b243b57c04b7c0b6dd4a03bf55b514d56b7ba165
ChrisTimperley <christimperley@googlemail.com>
2020-08-29 12:48:20 -0400

    added types module

---
+++ src/apodora/types.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+This module provides several type definitions for convenience.
+"""
+from typed_ast import ast27 as _ast27
+from typed_ast import ast3 as _ast3
+import typing as _t
+
+Py27AST = _ast27.AST
+Py3AST = _ast3.AST
