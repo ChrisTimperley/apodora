81ca1cbbb61e18609eab13f44f39587ba23ab2af
ChrisTimperley <christimperley@googlemail.com>
2020-08-29 16:07:45 -0400

    tidied Py27 and Py3 programs

--- src/apodora/program.py
+++ src/apodora/program.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 __all__ = ('Program', 'Py27Program', 'Py3Program')
 
-from typing import AbstractSet, Callable, Generic, Mapping, TypeVar
+from types import MappingProxyType
 from typed_ast import ast27, ast3
+from typing import (AbstractSet, Callable, Generic, Mapping, MutableMapping,
+                    TypeVar)
 import abc
 import functools
 import types
 
 import attr
 
-from .models import Module
+from .models import Module, Py27Module, Py3Module
 
 T = TypeVar('T', ast27.AST, ast3.AST)
 
@@ -29,12 +31,18 @@ class Program(Generic[T], abc.ABC):
         program. For now, we do not consider method entrypoints.
     """
     python: str = attr.ib(validator=attr.validators.instance_of(str))
-    modules: Mapping[str, Module] = attr.ib(repr=False, factory=dict)
+    modules: Mapping[str, Module] = attr.ib(repr=False, init=False)
+    _modules: MutableMapping[str, Module] = attr.ib(repr=False, init=False)
     main_module: str = attr.ib(default='__main__')
 
-    @classmethod
-    def from_sources(cls,
-                     python: str,
+    def __attrs_post_init__(self) -> None:
+        modules: MutableMapping[str, Module] = {}
+        read_only_modules: Mapping[str, Module] = MappingProxyType(modules)
+        object.__setattr__(self, '_modules', modules)
+        object.__setattr__(self, 'modules', read_only_modules)
+
+    @staticmethod
+    def from_sources(python: str,
                      module_to_source: Mapping[str, str],
                      main_module: str = '__main__'
                      ) -> 'Program':
@@ -49,17 +57,19 @@ class Program(Generic[T], abc.ABC):
             m = f"source code must be provided for main module: {main_module}"
             raise ValueError(m)
 
+        program: Program
         if python.startswith('2.'):
-            program_kls = Py27Program
+            program = Py27Program(python=python, main_module=main_module)
         elif python.startswith('3.'):
-            program_kls = Py3Program
+            program = Py3Program(python=python, main_module=main_module)
         else:
             raise ValueError(f"unsupported Python version: {python}")
 
-        program = program_kls(python=python, main_module=main_module)
+        # TODO introduce a proper module loader
         for name, source in module_to_source.items():
-            module = Module(program=program, name=name, source=source)
+            module = program.load_module(name, source)
             program.add_module(module)
+
         return program
 
     @property
@@ -72,10 +82,15 @@ class Program(Generic[T], abc.ABC):
     def is_py3(self) -> bool:
         ...
 
+    @abc.abstractmethod
+    def load_module(self, name: str, source: str) -> Module:
+        """Loads a given module from source."""
+        ...
+
     def add_module(self, module: Module) -> None:
         """Registers a given module with this program."""
         assert module.program == self
-        self.modules[module.name] = module
+        self._modules[module.name] = module
 
 
 class Py27Program(Program[ast27.AST]):
@@ -88,6 +103,10 @@ class Py27Program(Program[ast27.AST]):
     def is_py3(self) -> bool:
         return False
 
+    def load_module(self, name: str, source: str) -> Module:
+        """Loads a given module from source."""
+        return Py27Module(program=self, name=name, source=source)
+
 
 class Py3Program(Program[ast3.AST]):
     """Describes a Python 3 program."""
@@ -98,3 +117,7 @@ class Py3Program(Program[ast3.AST]):
     @property
     def is_py3(self) -> bool:
         return True
+
+    def load_module(self, name: str, source: str) -> Module:
+        """Loads a given module from source."""
+        return Py3Module(program=self, name=name, source=source)
