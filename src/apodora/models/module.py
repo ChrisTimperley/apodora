5bc6c2da75ec6050d18c31168a7ed63e5f609632
ChrisTimperley <christimperley@googlemail.com>
2020-08-29 10:49:28 -0400

    added ast and imports to Module

--- src/apodora/models/module.py
+++ src/apodora/models/module.py
@@ -7,25 +7,32 @@ import typing
 from loguru import logger
 import attr
 
+from ..helpers import ImportVisitor
+
 if typing.TYPE_CHECKING:
-    from .program import Program
+    from ..program import Program
 
 
-@attr.s(slots=True, auto_attribs=True)
+@attr.s(slots=True, auto_attribs=True, frozen=True)
 class Module:
     program: 'Program'
     name: str
     source: str = attr.ib(repr=False)
-    imports: AbstractSet[str] = attr.ib(init=False, repr=False)
-    ast: Any = attr.ib(init=False, repr=False)
+    _imports: AbstractSet[str] = attr.ib(init=False, repr=False)
+    _ast: Any = attr.ib(init=False, repr=False)
     # TODO: add filepath
 
-    def __getattr__(self, name: str) -> Any:
-        if name == 'ast':
-            return self._compute_ast()
-        return super().__getattr__(name)
+    @property
+    def ast(self) -> Any:
+        if not hasattr(self, '_ast'):
+            logger.debug(f'computing AST for module: {self}')
+            object.__setattr__(self, '_ast', self.program.parse(self.source))
+        return self._ast
 
-    def _compute_ast(self) -> Any:
-        logger.debug(f'computing AST for module: {self}')
-        self.ast = self.program.parse(self.source)
-        return self.ast
+    @property
+    def imports(self) -> AbstractSet[str]:
+        if not hasattr(self, '_imports'):
+            visitor = ImportVisitor.for_module(self)
+            visitor.visit(self.ast)
+            object.__setattr__(self, '_imports', frozenset(visitor.imports))
+        return self._imports
