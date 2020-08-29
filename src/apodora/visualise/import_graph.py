dee1388dab5001f91599de9cb29c2cd06a9236d4
ChrisTimperley <christimperley@googlemail.com>
2020-08-29 11:18:47 -0400

    added ImportGraph visualiser

---
+++ src/apodora/visualise/import_graph.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import tempfile
+import typing
+
+import attr
+import graphviz
+
+if typing.TYPE_CHECKING:
+    from ..program import Program
+
+
+@attr.s(slots=True, auto_attribs=True)
+class ImportGraph:
+    _dot: graphviz.Digraph
+
+    @classmethod
+    def for_program(self, program: 'Program') -> 'ImportGraph':
+        dot = graphviz.Digraph(comment='Import Graph')
+
+        for module_name in program.modules:
+            dot.node(module_name, module_name)
+
+        for module in program.modules.values():
+            for import_name in module.imports:
+                dot.edge(module.name, import_name)
+
+        return ImportGraph(dot)
+
+    def render(self, filename: str, view: bool = False) -> None:
+        """Renders this graph to a given file and optionally view it."""
+        self._dot.render(filename, view=view)
+
+    def view(self) -> None:
+        """Views the graph."""
+        self._dot.view(tempfile.mktemp('.gv'))
