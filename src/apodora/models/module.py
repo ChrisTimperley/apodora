1ff9b25a4c728e96cc99d70910901aa8f37bdf7a
ChrisTimperley <christimperley@googlemail.com>
2020-08-29 13:44:12 -0400

    added methods to Module

--- src/apodora/models/module.py
+++ src/apodora/models/module.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 __all__ = ('Module',)
 
-from typing import AbstractSet, Any
+from types import MappingProxyType
+from typing import AbstractSet, Any, Mapping
 import typing
 
 from loguru import logger
 import attr
 
 from ..helpers import ImportVisitor
+from ..helpers import MethodCollector
 
 if typing.TYPE_CHECKING:
     from ..program import Program
 
 
+# TODO add Py27Module and Py3Module
 @attr.s(slots=True, auto_attribs=True, frozen=True)
 class Module:
     program: 'Program'
@@ -20,6 +23,7 @@ class Module:
     source: str = attr.ib(repr=False)
     _imports: AbstractSet[str] = attr.ib(init=False, repr=False)
     _ast: Any = attr.ib(init=False, repr=False)
+    _methods: Mapping[str, Method] = attr.ib(init=False, repr=False)
     # TODO: add filepath
 
     @property
@@ -36,3 +40,12 @@ class Module:
             visitor.visit(self.ast)
             object.__setattr__(self, '_imports', frozenset(visitor.imports))
         return self._imports
+
+    @property
+    def methods(self) -> Mapping[str, Method]:
+        if not hasattr(self, '_methods'):
+            methods = MethodCollector.for_module(self)
+            name_to_method = {m.name: m for m in methods}
+            name_to_method = MappingProxyType(name_to_method)
+            object.__setattr__(self, '_methods', name_to_method)
+        return self._methods
