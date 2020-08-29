14d5adb87cae768926fb8d7dda0f05c75c21ce14
ChrisTimperley <christimperley@googlemail.com>
2020-08-29 15:40:55 -0400

    added additional import

--- src/apodora/models/__init__.py
+++ src/apodora/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from .block import BasicBlock, BlockNumbering
 from .method import Method, Py27Method, Py3Method
-from .module import Module
+from .module import Module, Py27Module, Py3Module
